0c792315647c51707dffef03b13715d4086ab834
Monique da Silva Borges <95103547+niqueborges@users.noreply.github.com>
2024-07-01 15:32:50 -0300

    Estudando Javascript

---
+++ App1/index.js
@@ -0,0 +1,18 @@
+console.log('Javascrip Funcionando');
+
+
+let firstName = 'Monique';
+let lastName = 'Silva';
+console.log(firstName);
+console.log(lastName);
+
+const price = 30;
+// price = 20;
+console.log(price);
+
+let itemNames = 'Camisa';
+let itemPrice = 30;
+let itemAvailable = true;
+let itemColor = 'Red';
+
+console.log(typeof itemPrice);
