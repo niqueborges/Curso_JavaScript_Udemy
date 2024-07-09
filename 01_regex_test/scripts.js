5dd1baaf3892f162d77ff5bc33bf26d4a7f7eca0
Monique da Silva Borges <95103547+niqueborges@users.noreply.github.com>
2024-07-09 15:48:07 -0300

    feat: exercícios

---
+++ 01_regex_test/scripts.js
@@ -0,0 +1,15 @@
+const reg1 = new RegExp('bola');
+
+console.log(reg1.test('Tem bola?'));
+console.log(reg1.test('Não tem!'));
+
+const reg2 = /bola/;
+
+let text = 'Tem bola na cesta.';
+
+console.log(reg2.test('Tem bola?'));
+console.log(reg2.test('Não tem!'));
+console.log(reg2.test(text));
+
+console.log(/quadrado/.test('Onde tem um quadrado?'));
+console.log(/quadrado/.test('343582457quadrado854554545254'));
