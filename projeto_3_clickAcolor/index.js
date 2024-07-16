2f73e44eee4ba9822efceeaaaf507bc4c72df137
Monique da Silva Borges <95103547+niqueborges@users.noreply.github.com>
2024-07-16 20:06:38 -0300

    Feat: projeto3

--- projeto_3_clickAcolor/index.js
+++ projeto_3_clickAcolor/index.js
@@ -4,4 +4,10 @@ const btn = document.getElementById('btn');
 
 btn.addEventListener('click', function() {
     document.body.style.background = colors[4];
+    const randomColor = getRandomColor();
+   // console.log(randomColor);
 });
+
+function getRandomColor() {
+    return Math.floor(Math.random() * colors.length);
+}
