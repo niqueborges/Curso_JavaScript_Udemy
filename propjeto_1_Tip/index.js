a536fb086ea1caf122e6cf05e53b5e1c0462a25a
Monique da Silva Borges <95103547+niqueborges@users.noreply.github.com>
2024-07-16 18:31:24 -0300

    Feat: ProjetoTip

---
+++ propjeto_1_Tip/index.js
@@ -0,0 +1,6 @@
+
+function update() {
+    let bill = Number(document.getElementById("yourBill").value);
+    let tipPercernt = document.getElementById("tipInput").value;
+    let split = document.getElementById("splitInput").value;
+
