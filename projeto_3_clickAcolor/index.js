e372b1f694c2f70c108ee294b8a4048f0d15f45c
Monique da Silva Borges <95103547+niqueborges@users.noreply.github.com>
2024-07-16 20:25:54 -0300

    Feat: projeto3_clickColor

--- projeto_3_clickAcolor/index.js
+++ projeto_3_clickAcolor/index.js
@@ -1,15 +1,15 @@
 
-const colors =['Red', 'black', 'Blue', 'Yellow', 'Purple', 'Green'];
-const btn = document.getElementById('btn');
-const color = document.querySelector('.color');
+const colors =['Red', 'black', 'Blue', 'Yellow', 'Purple', 'Green']; // Array com as cores
+const btn = document.getElementById('btn'); // Botão
+const color = document.querySelector('.color'); // Texto que exibe a cor
 
-btn.addEventListener('click', function() {
-    const randomColor = getRandomColor();
-    document.body.style.background = colors[randomColor];
+btn.addEventListener('click', function() { // Evento de click
+    const randomColor = getRandomColor(); // Função que gera uma cor aleatória
+    document.body.style.background = colors[randomColor]; // Altera a cor de fundo da página
    // console.log(randomColor);
-   color.textContent = colors[randomColor];
+   color.textContent = colors[randomColor]; // Altera o texto que exibe a cor
 });
 
-function getRandomColor() {
-    return Math.floor(Math.random() * colors.length);
+function getRandomColor() { // Função que gera uma cor aleatória
+    return Math.floor(Math.random() * colors.length); // Retorna uma cor aleatória entre 0 e o tamanho do array
 }
