54df85f27beabea131b179c313408867be259e97
Monique da Silva Borges <95103547+niqueborges@users.noreply.github.com>
2024-07-16 18:37:09 -0300

    Feat: projetotip

--- propjeto_1_Tip/index.js
+++ propjeto_1_Tip/index.js
@@ -4,3 +4,8 @@ function update() {
     let tipPercernt = document.getElementById("tipInput").value;
     let split = document.getElementById("splitInput").value;
 
+    let tipValue = bill * (tipPercernt / 100);
+    let billTotal = bill + tipValue;
+    let billEach = billTotal / split;
+
+}
