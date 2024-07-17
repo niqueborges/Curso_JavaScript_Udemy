1ac4b808b7f8530fcf43a2e2a5080a1e9769365b
Monique da Silva Borges <95103547+niqueborges@users.noreply.github.com>
2024-07-17 11:38:04 -0300

    Feat: projeto2

--- projeto_4_guess_the_number/index.js
+++ projeto_4_guess_the_number/index.js
@@ -1,8 +1,14 @@
 
-
 let computerNumber
+let userNumbers = []
 
 function init() {
     computerNumber = Math.floor(Math.random() * 100 + 1)
     console.log(computerNumber)
+}
+
+function compareNumber() {
+    const userNumber = Number(document.getElementById('inputBox').value)
+    userNumbers.push(' ' + userNumber)
+    document.getElementById('guesses').innerHTML = userNumbers
 }
