efeac3c0d14301181d93826676cafdb4fee39882
Monique da Silva Borges <95103547+niqueborges@users.noreply.github.com>
2024-07-17 12:00:45 -0300

    Update index.js

--- projeto_4_guess_the_number/index.js
+++ projeto_4_guess_the_number/index.js
@@ -4,11 +4,23 @@ let userNumbers = []
 
 function init() {
     computerNumber = Math.floor(Math.random() * 100 + 1)
-    console.log(computerNumber)
+    //console.log(computerNumber)
 }
 
 function compareNumber() {
     const userNumber = Number(document.getElementById('inputBox').value)
     userNumbers.push(' ' + userNumber)
     document.getElementById('guesses').innerHTML = userNumbers
+  
+    if (userNumber > computerNumber) {
+        document.getElementById('textOutput').innerHTML = 'Your number is too high'
+        document.getElementById('inputBox').value = ''
+    }
+    else if (userNumber < computerNumber) {
+        document.getElementById('textOutput').innerHTML = 'Your number is too low'
+        document.getElementById('inputBox').value = ''
+    }
+    else {
+        document.getElementById('textOutput').innerHTML = 'Congratulations! You guessed the number' 
+    }
 }
