143cd46c8afcf94ab3004f6cbc6e12ded296356e
Monique da Silva Borges <95103547+niqueborges@users.noreply.github.com>
2024-07-17 12:42:12 -0300

    Feat: projeto2

--- projeto_4_guess_the_number/index.js
+++ projeto_4_guess_the_number/index.js
@@ -1,10 +1,17 @@
 
 let computerNumber
 let userNumbers = []
+let attempts = 0
+let maxguesses = 10
+
+function newGame() {
+    window.location.reload()
+}
+
 
 function init() {
     computerNumber = Math.floor(Math.random() * 100 + 1)
-    //console.log(computerNumber)
+    console.log(computerNumber)
 }
 
 function compareNumber() {
@@ -12,15 +19,29 @@ function compareNumber() {
     userNumbers.push(' ' + userNumber)
     document.getElementById('guesses').innerHTML = userNumbers
   
-    if (userNumber > computerNumber) {
-        document.getElementById('textOutput').innerHTML = 'Your number is too high'
-        document.getElementById('inputBox').value = ''
-    }
-    else if (userNumber < computerNumber) {
-        document.getElementById('textOutput').innerHTML = 'Your number is too low'
-        document.getElementById('inputBox').value = ''
+    if (attempts < maxguesses) {
+        if (userNumber > computerNumber) {
+            document.getElementById('textOutput').innerHTML = 'Your number is too high'
+            document.getElementById('inputBox').value = ''
+            attempts++
+            document.getElementById('attempts').innerHTML = attempts
+        }
+        else if (userNumber < computerNumber) {
+            document.getElementById('textOutput').innerHTML = 'Your number is too low'
+            document.getElementById('inputBox').value = ''
+            attempts++
+            document.getElementById('attempts').innerHTML = attempts
+        }
+        else {
+            document.getElementById('textOutput').innerHTML = 'Congratulations! You guessed the number'
+            attempts++
+            document.getElementById('attempts').innerHTML = attempts
+            document.getElementById('inputBox').setAttribute('Readonly', 'Readonly') 
+        }
     }
     else {
-        document.getElementById('textOutput').innerHTML = 'Congratulations! You guessed the number' 
+        document.getElementById('textOutput').innerHTML = 'You Lose! The computer number was ' + computerNumber
+        document.getElementById('inputBox').setAttribute('Readonly', 'Readonly') 
+        }
     }
-}
+
